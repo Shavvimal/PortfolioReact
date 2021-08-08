d0ee07ab7cfeefb0710b79d28f2472e4bcc688ca
Shav <shavhugan@gmail.com>
2021-08-08 19:27:56 +0100

    stubbed out portfolio section

---
+++ src/pages/TechStack/index.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useRef} from 'react';
+import './style.css'
+import {  } from "./assets/animate.js"
+
+
+
+
+const TechStack = () => {
+   
+
+    return (
+
+<div class="bg-white raleway  my-20">
+
+  <div class="w-full h-1/2 flex flex-col gradientTech md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-44 overflow-hidden text-white">  
+  <h1 class="text-left text-4xl font-extrabold mt-12 w-1/2">My Tech Stack</h1>
+                        <h2 class="text-left text-2xl py-3 italic lg:w-1/2" > My preferred combination of programming languages, frameworks, and tools </h2>
+                        <p class="text-left text-xl lg:w-1/2"> I'm always looking to learn new technologies to widen my skillset. Here are some of the software and technologies that I’m already familiar with.  
+                        </p>
+        
+</div></div>
+
+    )
+
+
+}
+
+export default TechStack;
