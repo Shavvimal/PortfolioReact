1cc298fb6b953d0daf040a9ebc27c4eaff627659
Shav <shavhugan@gmail.com>
2021-08-13 00:07:09 +0100

    removed border and added coming soon to portfolio section

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -13,15 +13,18 @@ const Portfolio = () => {
 
 
 
-      <div id="portfolio" class=" border flex flex-col justify-center md:justify-start pt-8 md:pt-12 px-8 md:px-24  lg:px-44 h-1/2">
+      <div id="portfolio" class="flex flex-col justify-center md:justify-start pt-8 md:pt-12 px-8 md:px-24  lg:px-44 h-1/2">
         <h1 class="bodyheadercol text-left text-4xl font-extrabold">My Portfolio</h1>
                         <h2 class="text-left text-2xl py-3 italic" > Please feel free to check out some of my work! </h2>
                         <div class="flex">
                         <NavLink to="/Portfolio/Web" activeClassName="current  ">Web</NavLink>
                     <NavLink to="/Portfolio/Graphic" activeClassName="current">Graphic</NavLink> </div>
         
+                    <p class="m-auto"> Coming soon .... </p>
+        </div>
         
-        </div></>
+    
+        </>
     )
 
 
