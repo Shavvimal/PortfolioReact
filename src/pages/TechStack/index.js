897547d2f288864ae929db6db03288823893e37b
Shav <shavhugan@gmail.com>
2021-08-12 23:57:35 +0100

    changed vertical size of techsrack and added 'coming soon' for deploy

--- src/pages/TechStack/index.js
+++ src/pages/TechStack/index.js
@@ -1,24 +1,26 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import './style.css'
-import {  } from "./assets/animate.js"
+import { } from "./assets/animate.js"
 
 
 
 
 const TechStack = () => {
-   
+
 
     return (
 
-<div id="Techsect" class="bg-white raleway  my-20">
+        <div id="Techsect" class="bg-white raleway  my-20">
 
-  <div class="w-full h-1/2 flex flex-col gradientTech md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-44 overflow-hidden text-white">  
-  <h1 class="text-left text-4xl font-extrabold mt-12 w-1/2">My Tech Stack</h1>
-                        <h2 class="text-left text-2xl py-3 italic lg:w-1/2" > My preferred combination of programming languages, frameworks, and tools </h2>
-                        <p class="text-left text-xl lg:w-1/2"> I'm always looking to learn new technologies to widen my skillset. Here are some of the software and technologies that I’m already familiar with.  
-                        </p>
-        
-</div></div>
+            <div class="w-full h-5/6 flex flex-col gradientTech md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-44 overflow-hidden text-white">
+                <h1 class="text-left text-4xl font-extrabold mt-12 w-3/4">My Tech Stack</h1>
+                <h2 class="text-left text-2xl py-3 italic lg:w-3/5" > My preferred combination of programming languages, frameworks, and tools </h2>
+                <p class="text-left text-xl lg:w-3/5"> I'm always looking to learn new technologies to widen my skillset. Here are some of the software and technologies that I’m already familiar with.
+                </p>
+
+                <p class="m-auto"> Coming soon .... </p>
+
+            </div></div>
 
     )
 
