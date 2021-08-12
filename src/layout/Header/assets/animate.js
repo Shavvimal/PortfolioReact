88eedd0fb61691e52f4778bda4cdd181062a7662
Shav <shavhugan@gmail.com>
2021-08-12 23:59:33 +0100

    scrollTrigger animation added to header

--- src/layout/Header/assets/animate.js
+++ src/layout/Header/assets/animate.js
@@ -1,7 +1,21 @@
 import gsap from "gsap"
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
-export const initAnim = (elem1) => {
-    let tl = gsap.timeline({delay: 0});
-    tl.from(elem1, {duration: 0.6, ease: "power2. out", y:-69});
-    }
+
+
+export const finalAnim = (elem1) => {
+    let tl1 = gsap.timeline();
+    tl1.to(elem1, {
+        scrollTrigger: {
+            trigger: elem1,
+            start: "700px 50%",
+            end: "+=300px 40%",
+            scrub: 0.5,
+            // markers: true,
+        },
+        duration: 300,
+        ease: "power2. out",
+        y: -69,
+    });
+}
